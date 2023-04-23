0a129be007b24966eaf475e7823ee1f5a9a8a5d0
Marcin Kłak <klak.marcin01@gmail.com>
2023-04-23 15:01:58 +0200

    Dodanie pliku z laboratoriów main.cpp

--- Lab5.cpp
+++ Lab5.cpp
@@ -1,8 +1,21 @@
+#include "Wektor.h"
 #include<iostream>
+#include<string>
 
 using namespace std;
 
 int main() {
-    cout << "Marcin Klak";
-    return 0;
+	Wektor a(3), b(2);
+	a.wpisz();
+	b.wpisz();
+	
+    Wektor c = a + b; //wywolanie funkcji/metody operator+()
+	cout << endl << "c: " << c;
+	a += c;       //wywolanie funkcji/metody operator+=()
+	cout << endl << "a: " << a;
+	b = a;        //wywolanie metody operator=()
+	cout << endl << "b: " << b; //wywolanie metody operator<<()
+	Wektor d = a + b + c;
+	cout << endl << "d: " << d ; //wywolanie metody operator<<()
 }
+
