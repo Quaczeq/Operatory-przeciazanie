0d47495f9945896ea700becbe7451d9c1bf733d8
Marcin Kłak <klak.marcin01@gmail.com>
2023-04-23 14:27:27 +0200

    Dodanie plików .cpp i .h do zadania 5.

---
+++ Wektor.cpp
@@ -0,0 +1,57 @@
+#include "Wektor.h"
+using namespace std;
+Wektor::Wektor(int rozmiar) {
+	this->rozmiar = rozmiar;
+	wspolrzedne = new float[rozmiar];
+}
+Wektor::Wektor(const Wektor & w) {//definicja konstruktora kopiujacego
+	wspolrzedne = new float[rozmiar = w.rozmiar];//utworzenie tablicy dynamicznej o rzmiarze jak w w 
+	for (int i = 0; i < rozmiar; i++) {
+		wspolrzedne[i] = w.wspolrzedne[i]; //przepisanie wartosci z w
+	}
+}
+Wektor::~Wektor() {
+	delete[] wspolrzedne;
+}
+
+Wektor& Wektor::operator+(const Wektor & w) {//operator+ jako metoda 
+	int r = rozmiar < w.rozmiar ? rozmiar : w.rozmiar;
+	Wektor * tmp = new Wektor(r);//wektor wynikowy ma rozmiar miejszego z dodawanych wektorów
+	for (int i = 0; i < r; i++) {
+		tmp->wspolrzedne[i] = wspolrzedne[i] + w.wspolrzedne[i];
+	}
+	return *tmp;
+}
+
+void  Wektor::operator+=(const Wektor& w) {//operator+ jako metoda 
+	int r = rozmiar < w.rozmiar ? rozmiar : w.rozmiar;
+	for (int i = 0; i < r; i++) {
+		wspolrzedne[i] += w.wspolrzedne[i];
+	}
+}
+
+Wektor& Wektor::operator =(const Wektor & w) {
+	if (this != &w) {
+		if (rozmiar != w.rozmiar) {
+			delete[] wspolrzedne;
+			wspolrzedne = new float[rozmiar = w.rozmiar]; //tworzymy wektor o rozmiarze wektora w
+		}
+		for (int i = 0; i < rozmiar; i++)
+			wspolrzedne[i] = w.wspolrzedne[i];
+	}
+	return *this;
+}
+
+void Wektor::wpisz() {
+	cout << "\nPodaj wspolrzedne (" << rozmiar << "): ";
+	for (int i = 0; i < rozmiar; i++) {
+		cin >> wspolrzedne[i];
+	}
+
+}
+
+ostream & operator<<(ostream & o, const Wektor & w){
+	for (int i = 0; i < w.rozmiar; i++)
+		o << w.wspolrzedne[i] << " ";
+	return o;
+}
---
+++ Wektor.h
@@ -0,0 +1,20 @@
+#pragma once
+
+//E. Figielska, Metody programowania, 2019
+//Edit: M. Klak, 2023
+
+#include<iostream>
+class Wektor {
+	friend std::ostream & operator<<(std::ostream &, const Wektor &);	
+	float * wspolrzedne;
+	int rozmiar;
+public:
+	Wektor() {}
+	Wektor(int rozmiar);
+	Wektor(const Wektor &);
+	~Wektor();
+	Wektor& operator+(const Wektor &);
+	void operator+=(const Wektor&);
+	Wektor& operator =(const Wektor &);
+	void wpisz();
+};
